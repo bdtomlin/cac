5ccb12ec2dfa357258ee774e4fb45bf5d93dbe53
bryantomlin <bt@bryantomlin.com>
2014-08-02 18:33:30 -0500

    small refactor on alerts-service_spec

--- app/cac-app/alerts/alerts-service_spec.js
+++ app/cac-app/alerts/alerts-service_spec.js
@@ -1,19 +1,20 @@
 describe('AlertsSvc', function () {
+  var alert = {type: 'error', message: 'my message'};
+  
   beforeEach(module('cacApp.alerts'));
 
   it('should return an empty array for alerts', inject(function(AlertsSvc){
+      expect(Array.isArray(AlertsSvc.alerts)).toBe(true);
       expect(AlertsSvc.alerts.length).toBe(0);
   }));
 
   it('.addAlert should add an alert to the alerts array', inject(function(AlertsSvc){
-    var alert = {type: 'error', message: 'my message'};
     AlertsSvc.addAlert(alert);
     expect(AlertsSvc.alerts.length).toBe(1);
     expect(AlertsSvc.alerts[0]).toBe(alert);
   }));
 
   it('.dismissAlert should remove the alert from the alerts array', inject(function(AlertsSvc){
-    var alert = {type: 'error', message: 'my message'};
     AlertsSvc.addAlert(alert);
     AlertsSvc.dismissAlert(alert);
     expect(AlertsSvc.alerts.length).toBe(0);
