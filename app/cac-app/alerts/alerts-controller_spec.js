7dc7d75c89e074a88484dddb232cec67ad163ea4
bryantomlin <bt@bryantomlin.com>
2014-08-02 18:19:16 -0500

    wrote alerts-controller specs

--- app/cac-app/alerts/alerts-controller_spec.js
+++ app/cac-app/alerts/alerts-controller_spec.js
@@ -1,8 +1,21 @@
 describe("cacApp.alerts", function () {
+  var mockedAlertsSvc;
+
   beforeEach(module('cacApp.alerts'));
+  beforeEach(module(function($provide) {
+    mockedAlertsSvc = {
+      alerts: [],
+      addAlert: jasmine.createSpy('addAlert'),
+      dismissAlert: jasmine.createSpy('dismissAlert')
+    };
+    $provide.value('AlertsSvc', mockedAlertsSvc);
+  }));
+
 
   describe("AlertsCtrl", function () {
-    var ctrl, scope;
+    var ctrl,
+        scope,
+        alert = {type: 'error', message: 'some message'};
     beforeEach(inject(function($controller, $rootScope){
       scope = $rootScope.$new();
       ctrl = $controller('AlertsCtrl',{
@@ -10,19 +23,22 @@ describe("cacApp.alerts", function () {
       });
     }));
 
+    it("loads up the AlertSvc.alerts array", function(){
+      expect(scope.alerts).toBe(mockedAlertsSvc.alerts);
+    });
+
     it("listens for alerts and adds them to alerts array", function () {
-      var alert = {type: 'error', message: 'some message'};
       scope.$broadcast('alert', alert);
-      expect(scope.alerts.length).toBe(1);
-      expect(scope.alerts[0]).toBe(alert);
+      expect(mockedAlertsSvc.addAlert.argsForCall[0][0]).toBe(alert);
+      expect(mockedAlertsSvc.addAlert).toHaveBeenCalled();
     });
 
     it("dismisses alerts", function () {
       var alert = {type: 'error', message: 'some message'};
       scope.$broadcast('alert', alert);
       scope.dismissAlert(alert);
-
-      expect(scope.alerts.length).toBe(0);
+      expect(mockedAlertsSvc.dismissAlert.argsForCall[0][0]).toBe(alert);
+      expect(mockedAlertsSvc.dismissAlert).toHaveBeenCalled();
     });
   });
 });
